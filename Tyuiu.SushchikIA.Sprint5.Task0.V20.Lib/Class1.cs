6eee991202903afe319721836be57327ecadb188
Igorase <garik.sushik@ya.ru>
2025-11-26 09:27:27 +0300

    Добавьте файлы проекта.

---
+++ Tyuiu.SushchikIA.Sprint5.Task0.V20.Lib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.SushchikIA.Sprint5.Task0.V20.Lib
+{
+    public class DataService : ISprint5Task0V20
+    {
+        public string SaveToFileTextData(int x)
+        {
+            return "oejif";
+        }
+    }
+}
---
+++ Tyuiu.SushchikIA.Sprint5.Task0.V20.Test/Test1.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.SushchikIA.Sprint5.Task0.V20.Lib;
+namespace Tyuiu.SushchikIA.Sprint5.Task0.V20.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
