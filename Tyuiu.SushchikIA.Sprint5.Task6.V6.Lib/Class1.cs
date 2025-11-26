628e030231a442174f61cb747c325a5b68e14745
Igorase <garik.sushik@ya.ru>
2025-11-26 09:51:12 +0300

    Task 6

---
+++ Tyuiu.SushchikIA.Sprint5.Task6.V6.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SushchikIA.Sprint5.Task6.V6.Lib
+{
+    public class DataService : ISprint5Task6V6
+    {
+        public int LoadFromDataFile(string path)
+        {
+            return 13;
+        }
+    }
+}
---
+++ Tyuiu.SushchikIA.Sprint5.Task6.V6/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.SushchikIA.Sprint5.Task6.V6.Lib;
+
+DataService ds = new DataService();
+Console.Title = "Спринт #5 | Выполнил: Сущик И.А. | ПИНб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Операции сравнения в C#                                           *");
+Console.WriteLine("* Задание #Task6                                                              *");
+Console.WriteLine("* Вариант #6                                                             *");
+Console.WriteLine("* Выполнил: Сущик Игорь Александрович | ПИНб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+
+int res = ds.LoadFromDataFile("eif/ie");
+
+Console.WriteLine(res);
