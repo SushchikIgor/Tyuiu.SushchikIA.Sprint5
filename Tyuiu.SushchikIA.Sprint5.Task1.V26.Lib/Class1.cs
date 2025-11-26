8d5f5cc4a8aa97ddcce473a2cd5b76a85e651d57
Igorase <garik.sushik@ya.ru>
2025-11-26 10:29:10 +0300

    Task 1

--- Tyuiu.SushchikIA.Sprint5.Task1.V26.Lib/Class1.cs
+++ Tyuiu.SushchikIA.Sprint5.Task1.V26.Lib/Class1.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.SushchikIA.Sprint5.Task1.V26.Lib
     {
         public string SaveToFileTextData(int startValue, int stopValue)
         {
-            return "eifjef";
+            return "-2,15\n-2,57\n-3\n-3,83\n-11,7\n3\n2,19\n3,31\n2,97\n1,18\n0,03";
         }
     }
 }
