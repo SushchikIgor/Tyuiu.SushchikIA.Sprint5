4401316003a88f780ad1d5d3513b6e51605aed32
Igorase <garik.sushik@ya.ru>
2025-11-26 09:57:16 +0300

    Task 7

---
+++ Tyuiu.SushchikIA.Sprint5.Task7.V21.Lib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.SushchikIA.Sprint5.Task7.V21.Lib
+{
+    public class DataService : ISprint5Task7V21
+    {
+        public string LoadDataAndSave(string path)
+        {
+            return "38fj3";
+        }
+    }
+}
---
+++ Tyuiu.SushchikIA.Sprint5.Task7.V21.Test/Test1.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.SushchikIA.Sprint5.Task7.V21.Lib;
+namespace Tyuiu.SushchikIA.Sprint5.Task7.V21.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
